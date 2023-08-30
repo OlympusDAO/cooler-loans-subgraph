383615a2a321f65ec19e429865b1ffc012e6aa32
Jem <0x0xjem@gmail.com>
2023-08-30 15:39:12 +0400

    TODOs

--- src/cooler-factory.ts
+++ src/cooler-factory.ts
@@ -13,22 +13,15 @@ import {
 // ======================= Handle loan requests ======================= //
 
 export function handleClearRequest(event: ClearRequestEvent): void {
-  // let entity = new ClearRequest(
-  //   event.transaction.hash.concatI32(event.logIndex.toI32())
-  // )
-  // entity.cooler = event.params.cooler
-  // entity.reqID = event.params.reqID
-
-  // entity.blockNumber = event.block.number
-  // entity.blockTimestamp = event.block.timestamp
-  // entity.transactionHash = event.transaction.hash
-
-  // entity.save()
+  // Create a new CoolerLoan
 }
 
 // ======================= Handle loan actions ======================= //
 
 export function handleDefaultLoan(event: DefaultLoanEvent): void {
+  // Create new CoolerLoanSnapshot
+
+
   let entity = new DefaultLoan(
     event.transaction.hash.concatI32(event.logIndex.toI32())
   )
@@ -43,6 +36,8 @@ export function handleDefaultLoan(event: DefaultLoanEvent): void {
 }
 
 export function handleRepayLoan(event: RepayLoanEvent): void {
+  // Create new CoolerLoanSnapshot
+
   let entity = new RepayLoan(
     event.transaction.hash.concatI32(event.logIndex.toI32())
   )
@@ -58,6 +53,12 @@ export function handleRepayLoan(event: RepayLoanEvent): void {
 }
 
 export function handleRollLoan(event: RollLoanEvent): void {
+  // Create new CoolerLoan
+
+  // Create new CoolerLoanSnapshot for rollover
+
+  // Create new CoolerLoanSnapshot for previous loan
+
   let entity = new RollLoan(
     event.transaction.hash.concatI32(event.logIndex.toI32())
   )

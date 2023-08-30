e5026eca92d11e9a7b819f423aa4b6078f86edec
Jem <0x0xjem@gmail.com>
2023-08-30 15:29:09 +0400

    Simplify by removing loan requests

--- src/cooler-factory.ts
+++ src/cooler-factory.ts
@@ -1,33 +1,15 @@
 import {
-  ClearRequest as ClearRequestEvent,
   DefaultLoan as DefaultLoanEvent,
   RepayLoan as RepayLoanEvent,
-  RequestLoan as RequestLoanEvent,
-  RescindRequest as RescindRequestEvent,
   RollLoan as RollLoanEvent
 } from "../generated/CoolerFactory/CoolerFactory"
 import {
-  ClearRequest,
   DefaultLoan,
   RepayLoan,
-  RequestLoan,
-  RescindRequest,
   RollLoan
 } from "../generated/schema"
 
-export function handleClearRequest(event: ClearRequestEvent): void {
-  let entity = new ClearRequest(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.cooler = event.params.cooler
-  entity.reqID = event.params.reqID
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
+// ======================= Handle loan actions ======================= //
 
 export function handleDefaultLoan(event: DefaultLoanEvent): void {
   let entity = new DefaultLoan(
@@ -50,36 +32,6 @@ export function handleRepayLoan(event: RepayLoanEvent): void {
   entity.cooler = event.params.cooler
   entity.loanID = event.params.loanID
   entity.amount = event.params.amount
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleRequestLoan(event: RequestLoanEvent): void {
-  let entity = new RequestLoan(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.cooler = event.params.cooler
-  entity.collateral = event.params.collateral
-  entity.debt = event.params.debt
-  entity.reqID = event.params.reqID
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleRescindRequest(event: RescindRequestEvent): void {
-  let entity = new RescindRequest(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.cooler = event.params.cooler
-  entity.reqID = event.params.reqID
 
   entity.blockNumber = event.block.number
   entity.blockTimestamp = event.block.timestamp
